285c4ffcac6d6740918808517caa5a06096e994c
lenagend <lenagend@naver.com>
2021-09-23 10:21:15 +0900

    0923

---
+++ src/main/java/com/km/kmportfolio/KmPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.km.kmportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/km/kmportfolio/KmPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.kmportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
